730d03fab79a7942a254c43bf44a5ff1441105b1
Ilona Seaman <i.seaman@outlook.com>
2017-07-15 00:52:51 -0400

    added additional exercises and command line

---
+++ CommandLine_Exercises/DecimalToBinary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinary
+{
+    class Program
+    {
+        /*
+         This command-line program accepts a series of decimal integer values as command line arguments,
+         and displays each decimal value as itself and its binary equivalent.
+         */
+        static void Main(string[] args)
+        {
+            //create user-prompt for decimal values input
+            //and parse values-input to string of double values.
+            Console.WriteLine("Please enter in a series of decimal values:  ");
+            string[] nums = Console.ReadLine().Split(' ');
+
+            //create loop to convert values to binary equivalent, and print each in a separate line
+            for (int i=0; i<nums.Length; i++)
+            {
+                int baseTen = int.Parse(nums[i]);
+                string binaryEquiv = "";
+                while (baseTen > 0)
+                {
+                    binaryEquiv = (baseTen % 2) + binaryEquiv;
+                    baseTen = baseTen / 2;
+                }
+                Console.WriteLine($"{nums[i]} in binary is {binaryEquiv}");
+            }
+        }
+    }
+}
---
+++ CommandLine_Exercises/LinearConversion/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearConversion
+{
+    class Program
+    {
+        /*
+         This command-line program prompts user to enter a length, and whether its in (m)eters or (f)eet.
+         Convert the length to the opposite measurement, and display the old & new measurements.
+         The foot to meter conversion formula is:
+            m = f * 0.3048
+         The meter to foot conversion formula is:
+            f = m * 3.2808399
+         */
+        static void Main(string[] args)
+        {
+            //create user-prompt for length measurement
+            Console.WriteLine("Please enter the length:  ");
+            //and parse length-input to double
+            double userLength = double.Parse(Console.ReadLine());
+
+            //create user-input for measurement type
+            Console.WriteLine("Enter 'm' for meter, or 'f' for feet:  ");
+            //and parse type-input to char
+            char lengthType = char.Parse(Console.ReadLine());
+
+            //create if-method for meters-2-feet conversion
+            if (lengthType == 'm')
+            {
+                double f = userLength * 3.2808399;
+                //print display of old measurements and new
+                Console.WriteLine($"{userLength}m is {f}f.");
+            }
+            //create if-method for feet-2-meters conversion
+            else if (lengthType == 'f')
+            {
+                double m = userLength * 0.3048;
+                //print display of old measurements and new
+                Console.WriteLine($"{userLength}f is {m}m.");
+            }
+            //Write else-statement for invalid choices
+            else
+            {
+                Console.WriteLine(lengthType + " is an invalid choice.");
+            }
+        }
+    }
+}
